04c3b373b6dc3afa9d1c47637c108a3f25221313
moritzbe <moritzberthold@hotmail.com>
2015-02-15 14:39:08 +0100

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+user = User.create(name: "Reto")
+user1 = User.create(name: "Gavrillo")
+user2 = User.create(name: 'Lee')
+user3 = User.create(name: "Magellano")
+user4 = User.create(name: "Chris")
+user5 = User.create(name: "Loco")
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
